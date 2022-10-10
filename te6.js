f01afedc9548149ec13acdd896cdafbd961d3527
hanbee17 <hanbee.cho@sjsu.edu>
2022-10-10 13:20:30 -0700

    1

--- te6.js
+++
@@ -1,4 +0,0 @@
-console.log('Hello, this means I am working well. :)');
-let text = 'Hi, How are you? (I am a value :) )';
-console.log(text);
-text = 7;
---
+++ triangle.js
@@ -0,0 +1,3 @@
+console.log("Hi");
+for (let number = 0; number <= 10; number = +1);
+console.log(number);
