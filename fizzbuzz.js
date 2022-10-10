6f90be9ae4a12f89baa15387b86cdb01fe7c2a6e
hanbee17 <hanbee.cho@sjsu.edu>
2022-10-10 14:22:31 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,9 +2,10 @@ console.log("fizzbuzz");
 for (let number = 1; number <= 100; number ++) {
  if (number % 3 == 0) {
  console.log("Fizz");
-} 
-  if (number % 5 == 0) {
+}
+   else if (number % 5 == 0) {
   console.log("Buzz");
- }
+} else {
   console.log(number);
+}
 }
